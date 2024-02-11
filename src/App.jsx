ba12438a9f9cabe6774b7ae484ec479ef60ec98a
Shaun <shaun@Kellys-MacBook-Pro.local>
2024-02-11 14:35:43 -0500

    ready to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
+import './App.css'
 
 
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    // some logic here
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
+  console.log(featuredPup)
   return (
-    <>
-      <div>
-      </div>
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      <p>      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      </p>
+    </div>
+  );
+}
 
-
+export default App
